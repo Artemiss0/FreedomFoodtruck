6d04180941efa1d26bf4307131584f2a2ba8ca9b
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2020-04-14 16:59:51 +0200

    screen1
    added screen 1 and required to index, added styles links in index

---
+++ components/screen1.php
@@ -0,0 +1,5 @@
+<h1>Freedom<br>Foodtruck</h1>
+<img src="../images/lescargot.png"></img>
+<p>Houd je vinger vijf seconden<br>op de knop om te beginnen</p>
+<i class="fas fa-arrow-down"></i>
+<button>Klik hier om te bestellen</button>
--- components/startscherm.php
+++
@@ -1,5 +0,0 @@
-<h1>Freedom<br>Foodtruck</h1>
-<img></img>
-<p>Houd je vinger vijf seconden<br>op de knop om te beginnen</p>
-<img></img>
-<button></button>
