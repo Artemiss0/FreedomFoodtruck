11f696fddf1ac05f2ac265e95d5362ab6b3af56f
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2020-04-17 15:45:11 +0200

    screen4
    screen 4 is made

---
+++ components/screen4.php
@@ -0,0 +1,58 @@
+<div class="col-sm-12">
+    <h2 class="title title__center">Extra korting op je bestelling?</h2>
+    <div class="container container--white">
+            <p>Vul hier je gegevens ins</p>
+            <div class="container">
+                <!-- row1 -->
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="data">
+                            <h3>-25%</h3>
+                            <p>Foto van je gezicht</p>
+                        </div>
+                    </div>
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <h3>-10%</h3>
+                            <p>Social media</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- row2 -->
+                <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <h3>-10%</h3>
+                            <p>Telefoonnummer</p>
+                        </div>
+                    </div>
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <h3>-5%</h3>
+                            <p>Adres</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- row3 -->
+                <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <h3>-2%</h3>
+                            <p>E-mailadres</p>
+                        </div>
+                    </div>
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <h3>-2%</h3>
+                            <p>Je naam</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <a class="button button__primary--inverted" href="#"> Prev </a>
+                <a class="button button__primary" href="#"> Next </a>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -21,6 +21,10 @@
             <!-- screen3 -->
             <?php require_once 'components/screen3.php' ?>
         </div>
+        <div class="row">
+            <!-- screen4 -->
+            <?php require_once 'components/screen4.php' ?>
+        </div>
     <div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
