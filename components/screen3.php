89d17d7d10206236063da956cb1b56f52f1b79a1
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2020-04-17 15:16:56 +0200

    screen 3
    dummy icons have to be replaced still

---
+++ components/screen3.php
@@ -0,0 +1,58 @@
+<div class="col-sm-12">
+    <h2 class="title title__center">Tijdens je bestelling hebben we data<br>over jou verzameld</h2>
+    <div class="container container--white">
+            <p>Druk op de iconen om te zien hoe wij dit doen</p>
+            <div class="container">
+                <!-- row1 -->
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="data">
+                        <i class="fas fa-arrow-down"></i>
+                            <p>Geslacht</p>
+                        </div>
+                    </div>
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <i class="fas fa-arrow-down"></i>
+                            <p>BMI</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- row2 -->
+                <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <i class="fas fa-arrow-down"></i>
+                            <p>Aantrekkelijkheid</p>
+                        </div>
+                    </div>
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <i class="fas fa-arrow-down"></i>
+                            <p>Eetvoorkeur</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- row3 -->
+                <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <i class="fas fa-arrow-down"></i>
+                            <p>Hartslag</p>
+                        </div>
+                    </div>
+                        <div class="col-lg-5">
+                            <div class="data">
+                            <i class="fas fa-arrow-down"></i>
+                            <p>Leeftijd</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="button button__primary--inverted" href="#"> Prev </a>
+            <a class="button button__primary" href="#"> Next </a>
+        </div>
+    </div>
+</div>
